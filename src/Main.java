1f1be9e6ca4a4d11d2e6aa24e210d7f5158b5343
Alexandre Regalado <alexandreregalado@hotmail.com>
2021-11-30 17:36:11 +0000

    Estetica

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=============== Loja Online ===============");
         Loja loja = new Loja(18, "clientes.txt", "produtos.txt");
 
         System.out.println(loja.getClienteAtivo());
