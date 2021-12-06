ced1bafd78d85819e7fa179061c90fb8266ffc6f
Alex_pc <alexandreregalado3961@gmail.com>
2021-12-06 01:01:17 +0000

    reorganizacao de ficheiros

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("=============== Loja Online ===============");
-        Loja loja = new Loja("09-12-2021", "clientes.txt", "src/ficheiroProdutos");
+        Loja loja = new Loja("09-12-2021", "clientes.txt", "ficheiroProdutos");
 
         // System.out.println(loja.getClienteAtivo());
         loja.menu();
