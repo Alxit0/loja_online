d4f05092aa0f21b8f0b6518c73397bc15f752877
Alex_pc <alexandreregalado3961@gmail.com>
2021-12-08 21:15:49 +0000

    Melhoria da funcao main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
+import java.util.Scanner;
 
-
+/**
+ * Classe main onde o programa inicia
+ *
+ * @author Alexandre
+ * @author Bernardo
+ */
 public class Main {
+    private static final String FICHEIRO_PRODUTOS = "ficheiroProdutos";
+    private static final String FICHEIRO_CLIENTES = "ficheiroClientes";
+
     public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    /**
+     * Chamada de funcoes do programa
+     *
+     */
+    private void run(){
+        String data = pedirData();
         System.out.println("=============== Loja Online ===============");
-        Loja loja = new Loja("09-12-2021", "ficheiroClientes", "ficheiroProdutos");
+        Loja loja = new Loja(data, FICHEIRO_CLIENTES, FICHEIRO_PRODUTOS);
 
         // System.out.println(loja.getClienteAtivo());
         loja.menu();
 
         loja.exit("ficheiroClientes","ficheiroProdutos");
+    }
+
+    /**
+     * Funcao para pedir a data ao autilizador
+     *
+     * @return string com a data do dia em que o utiliza o programa
+     */
+    private String pedirData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Data atual (dd/mm/aaaa): ");
+        String data = sc.nextLine();
+
+        if(data.split("/").length != 3){
+            System.out.println("Formato invalido");
+        }
+
+        return data;
     }
 }
