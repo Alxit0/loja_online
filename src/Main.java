bb6e5cde678da858fc97a61fd4a2a2f0c2eb5758
Alexandre Regalado <alexandreregalado@hotmail.com>
2021-12-09 23:06:28 +0000

    correcao das datas

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,24 @@ public class Main {
             return pedirData();
         }
 
-        return data;
+        int mes = Integer.parseInt(data.split("/")[1]);
+        int dia = Integer.parseInt(data.split("/")[0]);
+        int ano = Integer.parseInt(data.split("/")[2]);
+        int eBisexto = 0;
+
+        if (ano%4==0 && (ano%100 != 0 || ano%400 == 0)) eBisexto = 1;
+
+        mes = Math.max(1, Math.min(12, mes));
+
+        if (mes == 2){
+            dia = Math.max(1, Math.min(28 + eBisexto, dia));
+        }else if ((mes%2==1 && mes < 8) || (mes%2==0 && mes > 7)){
+            dia = Math.max(1, Math.min(31, dia));
+        }else dia = Math.max(1, Math.min(30, dia));
+        String novaData = dia+"/"+mes+"/"+ano;
+
+        if (!novaData.equals(data)) System.out.println("Data invalida! Mudado para: "+novaData);
+
+        return dia+"/"+mes+"/"+ano;
     }
 }
