f770be9fd320c243d814b89d6b4ba553d4b030c6
Alexandre Regalado <alexandreregalado@hotmail.com>
2021-12-09 16:43:29 +0000

    ??

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
         // System.out.println(loja.getClienteAtivo());
         loja.menu();
 
-        loja.exit("ficheiroClientes","ficheiroProdutos");
+        loja.exit(FICHEIRO_CLIENTES, FICHEIRO_PRODUTOS);
     }
 
     /**
